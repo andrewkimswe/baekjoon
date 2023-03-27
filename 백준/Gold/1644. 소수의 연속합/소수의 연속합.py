350b80671563aa6d6bbd59e2f5c61377375a2bbe
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2023-03-27 16:31:12 +0900

    [Gold III] Title: 소수의 연속합, Time: 2000 ms, Memory: 95880 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,26 @@
+import math
+
+N = int(input())
+a = [False, False] + [True] * (N-1)
+prime = []
+
+for i in range(2, N+1):
+  if a[i]:
+    prime.append(i)
+    for j in range(2*i,N+1,i):
+      a[j] = False
+
+answer = 0;
+start = 0;
+end = 0;
+
+while end <= len(prime):
+  temp = sum(prime[start:end])
+  if temp == N:
+    answer += 1
+    end += 1
+  elif temp < N:
+    end += 1
+  else:
+    start += 1
+print(answer)
