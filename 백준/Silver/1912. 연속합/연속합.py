5e373c87db5a722e761a4271d85f570b5621b4ae
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2025-02-06 17:54:28 +0900

    [Silver II] Title: 연속합, Time: 80 ms, Memory: 39096 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+
+dp = [0] * n
+dp[0] = arr[0]
+
+for i in range(1, n):
+    dp[i] = max(arr[i], dp[i - 1] + arr[i])
+
+print(max(dp))
