e489494b38f7f8470962f69246ef02636e0d0f91
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2025-02-07 16:35:46 +0900

    [Silver I] Title: 오르막 수, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,12 @@
+N = int(input().strip())
+
+dp = [[0] * 10 for _ in range(N+1)]
+
+for j in range(10):
+    dp[1][j] = 1
+
+for i in range(2, N+1):
+    for j in range(10):
+        dp[i][j] = sum(dp[i-1][:j+1]) % 10007
+
+print(sum(dp[N]) % 10007)
